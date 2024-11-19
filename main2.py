d4cb146f8554c4a088be46a9edfc8fb05d71ea92
juan <juandavidrodriguezvargas7@gmail.com>
2024-11-18 21:41:31 -0500

    commit 2

--- main2.py
+++ main2.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Annotated
 from jose import jwt
+from pydantic import BaseModel 
 
 # Crear la aplicación FastAPI
 app = FastAPI()
 
+# Esquema de seguridad OAuth2
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+class  UserLogin(BaseModel):
+    username:str
+    password: str
+
 # Simulación de usuarios en memoria
 users = {
     "miguel": {"username": "miguel", "password": "123456", "email": "lopez@gmail.com"}
@@ -17,11 +24,16 @@ def encode_token(payload: dict) -> str:
     token = jwt.encode(payload, key="secret", algorithm="HS256")
     return token
 
-def  decode_token(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
-    data = jwt.decode(token, key:"secret", algorithm=["HS256"])
-    return data
-
-
+# Función para decodificar el token JWT
+def decode_token(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
+    try:
+        # Decodificamos el token usando la clave "secret" y el algoritmo "HS256"
+        data = jwt.decode(token, key="secret", algorithms=["HS256"])
+        return data
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="Invalid token")
 
 # Ruta de login
 @app.post("/login")
@@ -37,6 +49,27 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     # Devolver el token generado
     return {"access_token": token, "token_type": "bearer"}
 
+
+@app.post("/login_json")
+def login(form_data:UserLogin):
+    # Buscar el usuario en la base de datos simulada
+    user = users.get(form_data.username)
+    if not user or form_data.password != user["password"]:
+        raise HTTPException(status_code=404, detail="icorrect user or password")
+    
+    # Generar el token
+    token = encode_token({"username": user["username"], "email": user["email"]})
+
+    # Devolver el token generado
+    return {"access_token": token, "token_type": "bearer"}
+
+# Ruta de perfil de usuario (requiere autenticación)
 @app.get('/users/profile')
-def profile(my_user:Annotated[dict, Depends(decode_token)]):
+def profile(my_user: Annotated[dict, Depends(decode_token)]):
     return my_user
+
+
+
+@app.get('/users', dependencies=[Depends(decode_token)])
+def user_list():
+    return users
